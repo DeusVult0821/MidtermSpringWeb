045ad0a2b195b62739e6580e6905f835652db9fd
Erik Arancibia <erikarancibia2108@gmail.com>
2023-03-24 00:16:22 +0600

    first commit

---
+++ src/main/java/com/alatoo/midterm/MidtermSpringbootwebloginApplication.java
@@ -0,0 +1,13 @@
+package com.alatoo.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermSpringbootwebloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermSpringbootwebloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alatoo/midterm/MidtermSpringbootwebloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alatoo.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermSpringbootwebloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
